044e6b40e895c2544220fff1635389910d9c2e18
Chris Gavanas <christos.gavanas@agileactors.com>
2016-03-22 19:36:29 +0200

    checking merge

--- src/main/java/com/main/App.java
+++ src/main/java/com/main/App.java
@@ -5,4 +5,13 @@ public class App {
         System.out.println("Hello World!");
         System.out.println("Kalhspera kai kalh vradia.");
     }
+
+    public void test() {
+        int a = 2, b = 1;
+
+        if (a == 1) {
+            System.out.println(a);
+        } else
+            System.out.println(b);
+    }
 }
