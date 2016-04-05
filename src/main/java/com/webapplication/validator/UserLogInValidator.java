09a3f9588c853bc1f6898df9725998fa43793c4d
Feta <panagiotappl@gmail.com>
2016-04-05 19:35:26 +0300

    final login changes

--- src/main/java/com/webapplication/validator/UserLogInValidator.java
+++ src/main/java/com/webapplication/validator/UserLogInValidator.java
@@ -30,15 +30,17 @@ public class UserLogInValidator implements Validator<UserLogInRequestDto> {
 				throw new ValidationException(UserLogInError.MISSING_DATA);
 		}
 
-		if (request.getEmail() != null)
+		if (request.getEmail() != null) {
 			if (Arrays.asList(request.getEmail(), request.getPassword()).stream().anyMatch(field -> {
 				return field.isEmpty();
 			}))
 				throw new ValidationException(UserLogInError.INVALID_DATA);
-			else if (Arrays.asList(request.getUsername(), request.getPassword()).stream().anyMatch(field -> {
+		} else {
+			if (Arrays.asList(request.getUsername(), request.getPassword()).stream().anyMatch(field -> {
 				return field.isEmpty();
 			}))
 				throw new ValidationException(UserLogInError.INVALID_DATA);
+		}
 
 	}
 
