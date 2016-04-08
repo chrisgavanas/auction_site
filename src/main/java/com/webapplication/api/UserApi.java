c8146c1c5b02385410395bf9836065aebb54db74
Chris Gavanas <christos.gavanas@agileactors.com>
2016-04-08 17:42:02 +0300

    testing register platform

--- src/main/java/com/webapplication/api/UserApi.java
+++ src/main/java/com/webapplication/api/UserApi.java
@@ -1,16 +1,10 @@
 package com.webapplication.api;
 
-
-
-
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.webapplication.dto.UserIdRequestDto;
 import com.webapplication.dto.UserIdResponseDto;
 import com.webapplication.dto.UserLogInRequestDto;
 import com.webapplication.dto.UserLogInResponseDto;
@@ -21,13 +15,12 @@ import com.webapplication.dto.UserRegisterResponseDto;
 @RequestMapping(path = "/api")
 public interface UserApi {
 
-	@RequestMapping(path = "/login", method = RequestMethod.POST, consumes = "application/json", produces = "application/json")
-	UserLogInResponseDto login(UserLogInRequestDto userLogInRequestDto) throws Exception;
+    @RequestMapping(path = "/login", method = RequestMethod.POST, consumes = "application/json", produces = "application/json")
+    UserLogInResponseDto login(UserLogInRequestDto userLogInRequestDto) throws Exception;
 
-	@RequestMapping(path = "/user", method = RequestMethod.POST, consumes = "application/json", produces = "application/json")
-	UserRegisterResponseDto register(UserRegisterRequestDto userRegisterRequestDto) throws Exception;
-	
-	@RequestMapping(path = "/user/{userId}", method = RequestMethod.GET)
-	UserIdResponseDto getUserId(@PathVariable String userId) throws Exception;
+    @RequestMapping(path = "/user", method = RequestMethod.POST, consumes = "application/json", produces = "application/json")
+    UserRegisterResponseDto register(UserRegisterRequestDto userRegisterRequestDto) throws Exception;
+
+    @RequestMapping(path = "/user/{userId}", method = RequestMethod.GET, produces = "application/json")
+    UserIdResponseDto getUserId(@PathVariable String userId) throws Exception;
 }
-	
