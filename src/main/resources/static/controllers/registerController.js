2f42cec5c908a7cf09d6fb010417149dd3693d94
Feta <panagiotappl@gmail.com>
2016-04-09 18:28:19 +0300

    loginPassword

--- src/main/resources/static/controllers/registerController.js
+++ src/main/resources/static/controllers/registerController.js
@@ -2,8 +2,7 @@ var registerController = router.controller('registerController', function($scope
     $scope.user = {};
     $scope.register = function() {
         $scope.user.registrationDate = new Date();
-        if ($scope.user.username == '')
-            $scope.user.username = null;
+
         $http.post('/api/user', $scope.user).success((res) => {
             alert(res.error || "Success!");
         }).error(function(data, response) {
