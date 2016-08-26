30ce7649b8f9e36ebdf882680b8f9d6fb30da56d
Yiota <panagiotappl@gmail.com>
2016-08-26 14:44:02 +0300

    new

---
+++ src/main/resources/static/auctions/auction-item-offers.controller.js
@@ -0,0 +1,3 @@
+router.controller('offersController', function($state, $scope, $stateParams){
+	$scope.bidds  = [{bidder:"rulabula", amount: 3, date: new Date()}, {bidder:"kwtsohlias", amount: 4, date: new Date()}];
+});
