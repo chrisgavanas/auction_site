e5bee3629de17a7ac59fa06a92f6f0b5450d5cbc
chrisgavanas <chrisgavanas@gmail.com>
2016-09-24 17:08:37 +0300

    - Added recommendation stuff

--- src/main/java/com/webapplication/recommendation/Recommendation.java
+++ src/main/java/com/webapplication/recommendation/Recommendation.java
@@ -11,10 +11,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
-import java.util.HashSet;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
 
 @Component
@@ -26,6 +23,7 @@ public class Recommendation {
     private Integer k;
 
     private Map<String, Set<String>> preferredAuctionsPerUser = new ConcurrentHashMap<>();
+    private Map<String, Integer> bidsOrBuyoutPerAuction = new HashMap<>();
 
     @Autowired
     private UserRepository userRepository;
@@ -33,11 +31,14 @@ public class Recommendation {
     @Autowired
     private AuctionItemRepository auctionItemRepository;
 
-//    @Scheduled(fixedDelay = REFRESH_TIME)
+    @Scheduled(fixedDelay = REFRESH_TIME)
     private void recommendations() {
         if (k == null || k < 0)
             return;
+        long a = System.currentTimeMillis();
         preferredAuctionsPerUser.clear();
+        bidsOrBuyoutPerAuction.clear();
+
         List<User> users = userRepository.findAll();
         List<AuctionItem> auctionItems = auctionItemRepository.findAll();
         auctionItems.removeIf(auctionItem -> auctionItem.getBuyerId() == null && auctionItem.getBidsNo() == 0);
@@ -58,6 +59,35 @@ public class Recommendation {
                     break;
             }
         });
+        auctionItems.forEach(auctionItem -> bidsOrBuyoutPerAuction.put(auctionItem.getAuctionItemId(),
+                findNumberOfBidsOfAuctionItem(auctionItem.getAuctionItemId())));
+
+        System.out.println(similarity("57e2ae7a63448fff5384304a", "57e2ae7a63448fff538430b5"));
+        System.out.println(System.currentTimeMillis() - a);
+    }
+
+    private Integer findNumberOfBidsOfAuctionItem(String auctionItem) {
+        Optional<Integer> number = preferredAuctionsPerUser.values().stream()
+                .map(set -> set.contains(auctionItem) ? 1 : 0)
+                .reduce((c1, c2) -> c1 + c2);
+
+        if (number.isPresent())
+            return number.get();
+        else
+            return 0;
+    }
+
+    private Double similarity(String auctionItem1, String auctionItem2) {
+        Integer c1 = bidsOrBuyoutPerAuction.get(auctionItem1);
+        Integer c2 = bidsOrBuyoutPerAuction.get(auctionItem2);
+        Integer userLikesBothAuctions = 0;
+        for (Set<String> auctionsPerUser : preferredAuctionsPerUser.values()) {
+            Boolean userPreference1 = auctionsPerUser.contains(auctionItem1);
+            Boolean userPreference2 = auctionsPerUser.contains(auctionItem2);
+            userLikesBothAuctions += userPreference1 && userPreference2 ? 1 : 0;
+        }
+
+        return userLikesBothAuctions / Math.sqrt((c1 * c2));
     }
 
 }
